6c5e12d8d10d316cbb5696f6444c3bad90e6660a
Daniel Mechenko <dmechenko@gmail.com>
2021-09-04 18:11:16 -0400

    backend complete

---
+++ frontend/scribble.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Root from './components/root';
+// import configureStore from './store/store';
+// import * as SessionAPIUtil from './actions/session_actions';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // let store;
+  // if (window.currentUser) {
+  //   const preloadedState = {
+  //     entities: {
+  //       users: { [window.currentUser.id]: window.currentUser },
+  //     },
+  //     session: { id: window.currentUser.id },
+  //   };
+  //   store = configureStore(preloadedState);
+  //   delete window.currentUser;
+  // } else {
+  //   store = configureStore();
+  // }
+  // const store = configureStore();
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Test</h1>, root);
+});
