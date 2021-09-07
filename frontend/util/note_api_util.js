fd7693f1655bc7cffaa9bbbb7b4ca7295cc135e5
Daniel Mechenko <dmechenko@gmail.com>
2021-09-06 21:21:47 -0400

    noteUtil and noteActions

---
+++ frontend/util/note_api_util.js
@@ -0,0 +1,29 @@
+export const fetchNotes = () =>
+  $.ajax({
+    url: 'api/notes',
+  });
+
+export const fetchNote = (noteId) =>
+  $.ajax({
+    url: `/api/notes/${noteId}`,
+  });
+
+export const createNote = (note) =>
+  $.ajax({
+    url: `/api/notes`,
+    method: 'post',
+    data: { note },
+  });
+
+export const updateNote = (note) =>
+  $.ajax({
+    url: 'api/notes',
+    method: 'patch',
+    data: { note },
+  });
+
+export const deleteNote = (noteId) =>
+  $.ajax({
+    url: `/api/posts/${noteId}`,
+    method: 'delete',
+  });
