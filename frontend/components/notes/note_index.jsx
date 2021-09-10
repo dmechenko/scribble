749176f61b223fd905bd786f94fef33e2409308e
Daniel Mechenko <dmechenko@gmail.com>
2021-09-10 00:33:20 -0400

    added component did mount

--- frontend/components/notes/note_index.jsx
+++ frontend/components/notes/note_index.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import NoteLis from './note_li';
 
 class NoteIndex extends React.Component {
+  componentDidMount() {
+    this.props.fetchNotes();
+  }
   render() {
     return (
       <ul className='note-index-container'>
